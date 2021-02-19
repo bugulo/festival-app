fb31f2b706d565f8cd1e8059b862b98b599bda8a
René Lecső <xlecso00@vutbr.cz>
2021-02-19 16:19:18 +0000

    Merged PR 4: First basic entities addition
    Related work items: #9

---
+++ Database/Stage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database
+{
+    class Stage
+    {
+        public string StageName { get; set; } //Unique
+        public string NavRoute { get; set; } //Description to help visitors to find the stage
+        public string PhotosURL { get; set; } //URL attachment means string
+        public List<Band> StageBands { get; set; } = new List<Band>(); //List of Band classes (?)
+        public List<Slot> StageSlots { get; set; } = new List<Slot>(); //List of Slot classes (?)
+    }
+}
