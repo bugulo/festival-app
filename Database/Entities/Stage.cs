e762ff0f248fa27e5ad2c0a4994eca2f26b24728
Michal Šlesár <xslesa01@vutbr.cz>
2021-03-14 13:33:13 +0000

    Merged PR 5: Cleanup entities
    Related work items: #10

---
+++ Database/Entities/Stage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Database
+{
+    class Stage
+    {
+        public Guid Id { get; set; } // Unique identifier
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string PhotoURL { get; set; } // Photo of stage
+
+        public ICollection<Slot> Slots { get; set; } // Time slots for this stage tied to specific band
+    }
+}
