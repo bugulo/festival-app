aae04624d69ed5007528ca94b341bd719c5d4adc
Michal Šlesár <xslesa01@vutbr.cz>
2021-03-31 12:03:51 +0000

    Merged PR 8: Update structure for second iteration
    Related work items: #12

---
+++ Festival.BL.Tests/Placeholder.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace Festival.BL.Tests
+{
+    public class Placeholder
+    {
+        [Fact]
+        public void PlaceholderTest()
+        {
+            Assert.True(true);
+        }
+    }
+}
---
+++ Festival.DAL.Tests/Placeholder.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace Festival.DAL.Tests
+{
+    public class Placeholder
+    {
+        [Fact]
+        public void PlaceholderTest()
+        {
+            Assert.True(true);
+        }
+    }
+}
